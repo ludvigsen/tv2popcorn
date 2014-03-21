7c84925f0e579992600976bca5ad0ffddf959a6f
Thomas Sørheim Lund <thomas.sorheim.lund@gmail.com>
2014-03-21 14:17:57 +0100

    herjeherje

--- plugins/tv2article/popcorn.tv2article.js
+++ plugins/tv2article/popcorn.tv2article.js
@@ -14,6 +14,7 @@
                 element.css('z-index', options.zindex);
                 element.css('color', 'black');
                 element.css('width', 'auto');
+				element.addClass(options.class);
                 var target = Popcorn.dom.find( options.target );
                 if ( !target ) {
                     target = this.media.parentNode;
@@ -26,14 +27,14 @@
                         console.log('data: ', data);
                         if(data.result.length > 0){
                             var res = data.result[0];
-                            element.html("<img src='"+LOGO_URL+"' style='width:14px;margin-top:-1px;float:left;padding-right:5px;'/><a target=;_blank' href='"+res.published_url+"'>"+res.title+"</a>");
+                            element.html("<img src='"+LOGO_URL+"'><a target='"+ options.linktarget+"' href='"+res.published_url+"'>"+res.title+"</a>");
                         }
                     },
                     dataType: "jsonp"
                 });
                 options._container = element[0];
                 options._container.style.left = options.left + "%";
-                options._container.style.top = options.top + "%";
+                options._container.style.bottom = options.bottom + "%";
                 options._container.style.position = "absolute";
                 if ( options.width ) {
                     options._container.style.width = options.width + "%";
@@ -69,42 +70,53 @@
                 "label": "Out",
                 "units": "seconds"
             },
-        left: {
-          elem: "input",
-          type: "number",
-          label: "Left",
-          units: "%",
-          "default": 25,
-          hidden: true
-        },
-        top: {
-          elem: "input",
-          type: "number",
-          label: "Top",
-          units: "%",
-          "default": 0,
-          hidden: true
-        },
-        width: {
-          elem: "input",
-          type: "number",
-          units: "%",
-          label: "Width",
-          "default": 50,
-          hidden: true
-        },
-        zindex: {
-          hidden: true
-        },
-        articleId: {
-          elem: "input",
-          type: "text",
-          label: "Article URL"
-        }
-            /*,
-            "target": {
-                "hidden": false
-            }*/
-        }
+			left: {
+			  elem: "input",
+			  type: "number",
+			  label: "Left",
+			  units: "%",
+			  hidden: false,
+			  "default": 25
+			},
+			bottom: {
+			  elem: "input",
+			  type: "number",
+			  label: "Bottom",
+			  units: "%",
+			  hidden: false,
+			  "default": 10
+			},
+			width: {
+			  elem: "input",
+			  type: "number",
+			  units: "%",
+			  label: "Width",
+			  hidden: false,
+			  "default": 50
+			},
+			zindex: {
+			  hidden: true
+			},
+			articleId: {
+			  elem: "input",
+			  type: "text",
+			  label: "Article URL"
+			},
+			class:{
+				hidden: true,
+				"default": "tv2article"
+			},
+			linktarget: {
+				elem: "select",
+				options: [ "Samme vindu", "Nytt vindu"],
+				values: [ "_parent", "_blank"],
+				label: "Lenke åpnes i",
+				"default": "_blank"
+			}
+				/*,
+				"target": {
+					"hidden": false
+				}*/
+			}
     });
 }(Popcorn));
